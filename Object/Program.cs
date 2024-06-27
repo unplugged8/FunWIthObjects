36718c417d742614f88201c477c4b12c40899b25
unplugged8 <77389165+unplugged8@users.noreply.github.com>
2024-06-27 10:03:43 +0200

    Update README.md

--- Object/Program.cs
+++ Object/Program.cs
@@ -24,7 +24,7 @@ namespace ObjectCheckExample
 
                 Console.WriteLine("-----------------------------------------Nie, nie wszystko :) -------------------------------------------------");
                 Console.WriteLine($"\nOcena jaką dostane jesli pointer nie jest obiektem: {ocena} :)");
-                Console.WriteLine($"\nTa ocena {ocena} jest przechowywana w pamieci pod adresem {(int)ptr}, ktory widzimy dzieki pointer ptr.");
+                Console.WriteLine($"\nTa ocena {ocena} jest przechowywana w pamieci pod adresem {(int)ptr}, ktory widzimy dzieki pointer ptr i mozemy ja wyswietlic takze uzywajac pointer ptr: {*ptr}");
                 Console.WriteLine("\nPointer mozemy uzywac tylko po uprzednim dodaniu slowa 'unsafe' i po wlaczeniu opcji w:");
                 Console.WriteLine("\nProperties -> Build -> Unsafe code -> Allow code that uses the 'unsafe' keyword to compile.");
                 Console.WriteLine("\nNiestety nie mozna sprawdzic czy pointer jest obiektem uzywajac 'ptr is object',"); 
@@ -33,10 +33,10 @@ namespace ObjectCheckExample
                 Console.WriteLine("\nDefinicja: \n\nPointer types don't inherit from object and no conversions exist between pointer types and object. \n\nAlso, boxing and unboxing don't support pointers.");
                 Console.WriteLine("\n\n-------------------------------------------- Dziekuje :) ------------------------------------------------------");
 
-                //Odkomentować poniższy kod i najechać kursorem na ptr
+                //Odkomentowac ponizszy kod i najechac kursorem na ptr
                 //Console.WriteLine(ptr is object); // CS0244 ptr is pointer
 
-                //Odkomentować poniższy kod i najechać kursorem na ptr
+                //Odkomentowac ponizszy kod i najechac kursorem na ptr
                 //bool b = ptr is object;   // CS0244 ptr is pointer  
 
                 Console.ReadKey();
